67aa4c92ba079d9376b7dbdafb3859fca30d310e
곽현민 <hunmin111@gmail.com>
2026-01-29 16:13:39 +0900

    feat: level 1-2. 팀원 조회 기능

--- src/main/java/com/cloudintroteam/member/controller/MemberController.java
+++ src/main/java/com/cloudintroteam/member/controller/MemberController.java
@@ -1,14 +1,13 @@
 package com.cloudintroteam.member.controller;
 
 import com.cloudintroteam.member.dto.request.SaveMemberRequest;
+import com.cloudintroteam.member.dto.response.GetMemberResponse;
 import com.cloudintroteam.member.dto.response.SaveMemberResponse;
 import com.cloudintroteam.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +17,12 @@ public class MemberController {
     // 팀원 저장
     @PostMapping("/api/members")
     public ResponseEntity<SaveMemberResponse> saveMember(@RequestBody SaveMemberRequest request) {
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.save(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.save(request));
+    }
+
+    // 팀원 조회
+    @GetMapping("/api/members/{memberId}")
+    public ResponseEntity<GetMemberResponse> getMember(@PathVariable Long memberId) {
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.get(memberId));
     }
 }
--- src/main/java/com/cloudintroteam/member/service/MemberService.java
+++ src/main/java/com/cloudintroteam/member/service/MemberService.java
@@ -1,8 +1,10 @@
 package com.cloudintroteam.member.service;
 
 import com.cloudintroteam.member.dto.request.SaveMemberRequest;
+import com.cloudintroteam.member.dto.response.GetMemberResponse;
 import com.cloudintroteam.member.dto.response.SaveMemberResponse;
 import com.cloudintroteam.member.entity.Member;
+import com.cloudintroteam.member.exception.MemberNotFoundException;
 import com.cloudintroteam.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -27,5 +29,18 @@ public class MemberService {
                 savedMember.getName(),
                 savedMember.getAge(),
                 savedMember.getMbti());
+    }
+
+    // 팀원 조회
+    @Transactional(readOnly = true)
+    public GetMemberResponse get(Long memberId) {
+        Member member = memberRepository.findById(memberId).orElseThrow(
+                () -> new MemberNotFoundException("존재하지 않는 팀원입니다."));
+
+        return new GetMemberResponse(
+                member.getId(),
+                member.getName(),
+                member.getAge(),
+                member.getMbti());
     }
 }
